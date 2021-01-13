5f0bc4d9360180ec520f6b4630e5cd7d89cbbd95
BigSten <coolgirl25rus@mail.ru>
2021-01-13 20:10:45 +1000

    menu

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ menu.onclick = function myFunction() {
   if (x.className === "topnav") {
     x.className += " responsive";
     document.getElementById("inner").style.backgroundColor = "#000";
-    document.getElementById("inner").style.padding = "25px 4.6% 100vh 4.6%";
     document.getElementById("inner").style.position = "fixed";
     document.getElementById("inner").style.left = "50%";
     document.getElementById("inner").style.transform = "translate(-50%)";
     document.body.style.overflow = "hidden";
+    document.getElementById("inner").style.paddingBottom = "100vh";
+
   } else {
     x.className = "topnav";
     document.getElementById("inner").style.backgroundColor = "transparent";
-    document.getElementById("inner").style.padding = "25px 0 15px 0";
+    document.getElementById("inner").style.paddingBottom = "0";
     document.getElementById("inner").style.position = "relative";
     document.body.style.overflow = "visible";
   }
